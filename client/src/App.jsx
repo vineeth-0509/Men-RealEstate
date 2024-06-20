34ba530249c14ac06118ed3a4399aca33cdf27ac
vineeth-0509 <vineeththungani@gmail.com>
2024-06-21 05:05:08 +0530

    Installed react js and tailwind css and created first template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-blue-500'>
+      APP
+    </div>
+  )
+}
+
+export default App
