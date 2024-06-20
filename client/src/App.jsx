9e98f4822ec2bd6d27782799ae4c63027088ebcd
vineeth-0509 <vineeththungani@gmail.com>
2024-06-21 05:18:17 +0530

    created pages and addded routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-blue-500'>
-      APP
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/Home' element={<Home/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    <Route path='/signin' element={<SignIn/>}></Route>
+    <Route path='/signup' element={<SignUp/>}></Route>
+    <Route path='/profile' element={<Profile/>}></Route>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
