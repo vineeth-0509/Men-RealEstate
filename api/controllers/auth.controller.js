af60cf97ba814102f296871d9ae7cfa980505927
vineeth-0509 <vineeththungani@gmail.com>
2024-06-21 19:19:53 +0530

    created signin api route and jsonwebtoken

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 
 export const signup=async (req,res,next)=>{
@@ -12,8 +14,6 @@ export const signup=async (req,res,next)=>{
     } catch (error) {
       next(error);
     }
-
-  
 };
 
 //import { errorHandler } from "../utils/error.js";
@@ -24,3 +24,30 @@ Some cases in our application we need to have custom error.
         next(errorHandler(500,'error from the function signup in auth.controller.js'));
         }
     */
+
+        export const signin=async(req,res,next)=>{
+          const {email,password}=req.body;
+          try {
+           const validUser= await User.findOne({
+           email:email
+           });
+           if(!validUser){
+            return (errorHandler(404,'user not found!'));
+           }
+
+           const validPassword=bcryptjs.compareSync(password,validUser.password);
+           if(!validPassword){
+            return next(errorHandler(404,'password is incorrect'));
+           }
+           const token= jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+           //all the things except the password will get return and also with the cookie.
+           const {password: pass, ...rest}=validUser._doc;
+           res
+           .cookie('access-token',token,{httpOnly: true})
+           .status(200)
+           .json(rest);
+          }
+           catch (error) {
+            next(error);
+          }
+        }
