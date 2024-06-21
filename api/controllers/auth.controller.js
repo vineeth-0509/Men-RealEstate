dd4b00b32415b62b573825af29554b865f0206b4
vineeth-0509 <vineeththungani@gmail.com>
2024-06-21 15:24:53 +0530

    created signup with complete frontend and backend and mongodb vy giving the credentials

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,25 +1,26 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from "../utils/error.js";
+
+
 export const signup=async (req,res,next)=>{
-    const {userName, email,password}= req.body;
-    const hashPassword=bcryptjs.hashSync(password,10);
-    const newUser= new User({userName,email,password:hashPassword});
-    try{
-        await newUser.save()
-    res.status(201).json({
-        msg:"user created Successfully"
-    });
-    }catch(error){
-       // res.status(500).json(error.message)
-        next(errorHandler(500,error.message))
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+      await newUser.save();
+      res.status(201).json('User created successfully!');
+    } catch (error) {
+      next(error);
     }
 
-    /* 
+  
+};
+
+//import { errorHandler } from "../utils/error.js";
+  /* 
 Some cases in our application we need to have custom error.
     for custom error, we can use the 
         catch(err){
         next(errorHandler(500,'error from the function signup in auth.controller.js'));
         }
     */
-};
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React,{useState}from 'react'
 import {Link} from 'react-router-dom';
 
 export default function SignUp() {
+  const[formData,setFormData]=useState({});
+  const  handleChange=(e)=>{
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]:e.target.value
+      });
+  };
+
+
+  const handleSubmit= async (e)=>{
+    e.preventDefault();
+    const res= await fetch('/api/auth/signup',
+      {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(formData),
+      });
+      const data= await res.json();
+      console.log(data);
+  };
+ 
+  console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto' >
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-    <form className='flex flex-col gap-4' >
-      <input type="text" placeholder='username'  className='border p-3 rounded-lg' id='username'/>
-      <input type="email" placeholder='email'  className='border p-3 rounded-lg' id='email'/>
-      <input type="password" placeholder='password'  className='border p-3 rounded-lg' id='password'/>
+
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
+      <input type="text" placeholder='username'  className='border p-3 rounded-lg' id='username'onChange={handleChange} />
+      <input type="email" placeholder='email'  className='border p-3 rounded-lg' id='email'onChange={handleChange}/>
+      <input type="password" placeholder='password'  className='border p-3 rounded-lg' id='password'onChange={handleChange} />
       <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>sign Up</button>
     </form>
+
     <div className='flex gap-2 mt-5'>
       <p>Have an account?</p>
       <Link to='/signin'>
@@ -20,3 +47,4 @@ export default function SignUp() {
     </div>
   )
 }
+ 
