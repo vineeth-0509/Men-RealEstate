54e37cf202807ba72395e8edcc8170587d4736e8
vineeth-0509 <vineeththungani@gmail.com>
2024-06-21 09:29:39 +0530

    added middlewares for the errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup=async (req,res)=>{
+import { errorHandler } from "../utils/error.js";
+export const signup=async (req,res,next)=>{
     const {userName, email,password}= req.body;
     const hashPassword=bcryptjs.hashSync(password,10);
     const newUser= new User({userName,email,password:hashPassword});
@@ -10,14 +11,15 @@ export const signup=async (req,res)=>{
         msg:"user created Successfully"
     });
     }catch(error){
-        res.status(500).json(error.message)
+       // res.status(500).json(error.message)
+        next(errorHandler(500,error.message))
     }
 
     /* 
 Some cases in our application we need to have custom error.
     for custom error, we can use the 
         catch(err){
-        next(errorHandler(300, error.message));
+        next(errorHandler(500,'error from the function signup in auth.controller.js'));
         }
     */
 };
